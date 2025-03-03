63bc1c18b3ca65328553c5094e32b38e9b4c08af
SHIN SUJUNG <shinssjj79@gmail.com>
2025-03-03 09:56:45 +0900

    Initial commit

---
+++ src/main/java/com/example/RandomQuizGame/RandomQuizGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.RandomQuizGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuizGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuizGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RandomQuizGame/RandomQuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RandomQuizGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQuizGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
